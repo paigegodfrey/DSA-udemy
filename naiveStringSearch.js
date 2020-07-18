b2eacd4cb0cbddc0be24af7953a6defff2729c91
Paige Godfrey <paagodfrey@gmail.com>
2020-07-18 16:39:07 -0700

    initialize KMP search file

---
+++ naiveStringSearch.js
@@ -0,0 +1,11 @@
+function stringSearch(str, substring) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < substring.length; j++) {
+      if (substring[j] !== str[i + j]) break;
+      if (j === substring.length - 1) count++;
+    }
+  }
+  return count;
+}
