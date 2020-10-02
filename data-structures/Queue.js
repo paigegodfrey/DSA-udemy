2d378970ce64026f086110ff7232f5f49faaced7
Paige Godfrey <paagodfrey@gmail.com>
2020-10-02 10:50:59 -0700

    Queue init

---
+++ data-structures/Queue.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = first;
+    this.last = last;
+    this.size = 0;
+  }
+}
