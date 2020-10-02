f26695f435c94628c86c655fff4b27f36bf1d7e0
Paige Godfrey <paagodfrey@gmail.com>
2020-10-02 10:56:03 -0700

    Queueu add complete

--- data-structures/Queue.js
+++ data-structures/Queue.js
@@ -11,4 +11,18 @@ class Queue {
     this.last = last;
     this.size = 0;
   }
+
+  // works the same as push
+  add(val) {
+    let newNode = new Node(val);
+
+    if (!this.first) this.first = newNode;
+    else this.last.next = newNode;
+
+    this.last = newNode;
+    this.size++;
+    return this.size;
+
+  }
+
 }
