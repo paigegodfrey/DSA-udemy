017ef60a94b7f8c735bb328429f91369137358c1
Paige Godfrey <paagodfrey@gmail.com>
2020-10-02 08:14:36 -0700

    HashTableCheck set complete

--- data-structures/HashTableCheck.js
+++ data-structures/HashTableCheck.js
@@ -1,7 +1,27 @@
-class ListNode {
-  constructor(val) {
+class Node {
+  constructor(key, val) {
+    this.key = key;
     this.val = val;
     this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(key, val) {
+    let newNode = new Node(key, val);
+
+    if (!this.head) this.head = newNode;
+    else this.tail.next = newNode;
+
+    this.tail = newNode;
+    this.length++;
+    return this.length;
   }
 }
 
@@ -14,7 +34,7 @@ class HashTable {
     let total = 0;
     let PRIME = 31;
 
-    for(let i = 0; i < Math.min(str.length, 100); i++) {
+    for (let i = 0; i < Math.min(str.length, 100); i++) {
       value = key[i].charCodeAt(0) - 96;
       total += (total * PRIME + value) % this.keyMap.length;
     }
@@ -23,7 +43,12 @@ class HashTable {
   }
 
   set(key, value) {
-    
+    let idx = this._hash(key);
+    if (!this.keyMap[idx]) this.keyMap[idx] = new LinkedList;
+    this.keyMap[idx].push(key, value);
   }
 
+  get(key) {
+    
+  }
 }
