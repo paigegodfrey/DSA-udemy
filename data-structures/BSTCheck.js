7f2eb3b4e3d80d87dc4e2ca528d88bc88f6e6aed
Paige Godfrey <paagodfrey@gmail.com>
2020-10-02 12:18:55 -0700

    BST insert draft

--- data-structures/BSTCheck.js
+++ data-structures/BSTCheck.js
@@ -10,4 +10,33 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  // assume no duplicate values
+  insert(val) {
+    let nodeToInsert = new Node(val);
+
+    if (!this.root) {
+      this.root = nodeToInsert;
+      return;
+    }
+
+    let current = this.root;
+
+    while (current !== null) {
+      if (nodeToInsert > current) {
+        if (current.right) current = current.right;
+        else {
+          current.right = nodeToInsert;
+          return;
+        }
+      }
+      else if (nodeToInsert < current) {
+        if (current.left) current = current.left;
+        else {
+          current.right = nodeToInsert;
+          return;
+        }
+      }
+    }
+  }
 }
