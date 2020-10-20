bc6dda9364358b36462a7f6dd5c02f89254fce47
Paige Godfrey <paagodfrey@gmail.com>
2020-10-20 15:30:14 -0700

    BSTCheck insert

--- data-structures/BSTCheck.js
+++ data-structures/BSTCheck.js
@@ -12,7 +12,29 @@ class BST {
   }
 
   insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
 
+    let current = this.root;
+    while (true) {
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      }
+      else if (val > current.val) {
+        if (!current.right) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
   }
 
   find() {
