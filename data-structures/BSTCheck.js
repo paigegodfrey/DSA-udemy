5c5392837d13e6c9819a6741c62e0489309d4160
Paige Godfrey <paagodfrey@gmail.com>
2020-10-20 15:29:20 -0700

    BSTCheck init

---
+++ data-structures/BSTCheck.js
@@ -0,0 +1,21 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+
+  }
+
+  find() {
+
+  }
+}
