13f90a5995df59e26ad52a64b8f844313f1c0e0d
Paige Godfrey <paagodfrey@gmail.com>
2020-10-23 16:31:46 -0700

    TrieCheck init

---
+++ data-structures/TrieCheck.js
@@ -0,0 +1,25 @@
+class Node {
+  constructor() {
+    this.children = {};
+    this.wordComplete = false;
+  }
+}
+
+
+class Trie {
+  constructor() {
+    this.root = null;
+  }
+
+  insert() {
+
+  }
+
+  search() {
+
+  }
+
+  startsWith() {
+
+  }
+}
