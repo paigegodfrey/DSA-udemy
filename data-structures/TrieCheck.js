2c51524d0c22d045c0e79ec51645784a0f930b09
Paige Godfrey <paagodfrey@gmail.com>
2020-10-23 16:52:57 -0700

    TrieCheck draft

--- data-structures/TrieCheck.js
+++ data-structures/TrieCheck.js
@@ -11,15 +11,35 @@ class Trie {
     this.root = null;
   }
 
-  insert() {
-
+  insert(word) {
+    // define pointer (current) => start at root
+    let current = this.root;
+    for (let letter of word) {
+      if (!current.children[letter]) {
+        let newNode = new Node();
+        current.children[letter] = newNode;
+      }
+      current = current.children[letter];
+    }
+    current.wordComplete = true;
+    return;
   }
 
-  search() {
-
+  // returns boolean
+  search(word) {
+    let current = this.root;
+    for (let letter of word) {
+      if (!current.children[letter]) return false;
+      current = current.children[letter];
+    }
+    return current.wordComplete;
   }
 
-  startsWith() {
-
+  startsWith(prefix) {
+    let current = this.root;
+    for (let letter of prefix) {
+      if (!current.children[letter]) return false;
+    }
+    return current.children ? true : false;
   }
 }
